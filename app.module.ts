92e84e366ab9521aec65ff5b95274a2ddddbe58c
PierreTubeuf <64647464+PierreTubeuf@users.noreply.github.com>
2020-05-01 17:17:52 +0200

    http
    Fichier pour HTTP

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WebServiceService } from './web-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [WebServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  url:string = "https://jsonplaceholder.typicode.com/users";
+
+  GetPosts(){
+    return this.httpClient.get(this.url);           
+  }
+  vadump
+}
